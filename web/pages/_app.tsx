27bf492cd4a2e5f4a37c2c2dfa7087664b72ae55
jennyyfei <116273484+jennyyfei@users.noreply.github.com>
2022-12-25 13:48:53 +0100

    rm test

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,14 +2,11 @@ import { Analytics } from "@vercel/analytics/react";
 
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
       <Analytics />
     </>
   );
